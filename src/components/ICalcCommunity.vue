62d825ff64fb4bc767240d99a0e3e108f23032dd
LuceCompany <ninsebastianluc@gmail.com>
2020-09-03 02:27:09 +0200

    Add read keyboard option

--- src/components/ICalcCommunity.vue
+++ src/components/ICalcCommunity.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="Icalc">
 
-    <div class="display">
-      <div class="visible_element"> {{current || '0'}} </div> 
+    <div class="display"> <!-- To repair -->
+      <button v-on:keydown="numadd" class="visible_element"> {{current || '0'}} </button> 
     </div>
     
     <div class="top_operator">
@@ -53,6 +53,65 @@ export default {
     sign() {
       this.current = this.current.charAt(0) === '-' ?
         this.current.slice(1) : `-${this.current}`;
+    },
+    numadd: function(event) {
+      if(event.key == "Delete" || event.key == "Backspace"){
+        this.clear();
+      }
+      else if(event.key == "s"){
+        this.sign();
+      }
+      else if(event.key == "%"){
+        this.mod();
+      }
+      else if(event.key == "/"){
+        this.divide();
+      }
+      else if(event.key == "7"){
+        this.append("7");
+      }
+      else if(event.key == "8"){
+        this.append("8");
+      }
+      else if(event.key == "9"){
+        this.append("9");
+      }
+      else if(event.key == "*"){
+        this.times();
+      }
+      else if(event.key == "4"){
+        this.append("4");
+      }
+      else if(event.key == "5"){
+        this.append("5");
+      }
+      else if(event.key == "6"){
+        this.append("6");
+      }
+      else if(event.key == "-"){
+        this.minus();
+      }
+      else if(event.key == "1"){
+        this.append("1");
+      }
+      else if(event.key == "2"){
+        this.append("2");
+      }
+      else if(event.key == "3"){
+        this.append("3");
+      }
+      else if(event.key == "+"){
+        this.add();
+      }
+      else if(event.key == "0"){
+        this.append("0");
+      }
+      else if(event.key == "," || event.key == "." ){
+        this.append(".");
+      }
+      else if(event.key == "Enter" || event.key == "="){
+        this.equal();
+      }
     },
     append(number) {
       if (this.operatorClicked) {
